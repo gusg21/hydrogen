83c4069344750b00e55f97505f1484f40726f8d4
Gus (224) <angus.goucher@mymail.champlain.edu>
2024-08-12 20:09:23 -0400

    added window class WIP

--- core/include/core/window.h
+++ core/include/core/window.h
@@ -1,14 +1,15 @@
 #include <stdint.h>
+#include <SDL3/SDL.h>
 
 namespace h_core
 {
     class Window {
     public:
-        Window(const char* title, uint32_t width, uint32_t height);
+        Window(const char* title, uint32_t width, uint32_t height, bool fullscreen);
         Window(const Window&) = delete;
         Window& operator=(const Window&) = delete;
 
     private:
-        
+        SDL_Window* m_window;
     };
 } // namespace h_core
--- core/src/window.cc
+++ core/src/window.cc
@@ -1,7 +1,9 @@
-#include <core/window.h>
+#include "core/window.h"
 
 #include <stdint.h>
+#include <SDL3/SDL.h>
 
-h_core::Window::Window(const char* title, uint32_t width, uint32_t height) {
-    
+h_core::Window::Window(const char* title, uint32_t width, uint32_t height, bool fullscreen) {
+    SDL_Init(0);
+    m_window = SDL_CreateWindow(title, width, height, SDL_WINDOW_RESIZABLE);
 }
