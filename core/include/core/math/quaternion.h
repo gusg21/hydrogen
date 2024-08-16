2728f63fc2391f1e1e08b50f2f11b4e5691304c2
Angus Goucher [CCM 224 2] <gus.goucher@gmail.com>
2024-08-16 18:24:56 -0400

    quaternion spelling fix

--- core/include/core/math/quaternion.h
+++ core/include/core/math/quaternion.h
@@ -2,9 +2,9 @@
 
 namespace h_core {
 namespace math {
-struct Quaternian {
-    Quaternian(float value) : x(value), y(value), z(value), w(value) {}
-    Quaternian(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+struct Quaternion {
+    Quaternion(float value) : x(value), y(value), z(value), w(value) {}
+    Quaternion(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
 
     float x;
     float y;
