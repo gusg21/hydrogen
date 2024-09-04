8d5530aaf06501b199f9949618d4d09495315027
Xavier Olmstead <xavier.olmstead@mymail.champlain.edu>
2024-09-04 16:43:12 -0400

    beginning work on physics system

---
+++ core/include/core/physics/rigidbody.h
@@ -0,0 +1,19 @@
+//
+// Created by xavier.olmstead on 9/4/2024.
+//
+
+#pragma once
+
+#include "core/system.h"
+
+
+namespace h_core {
+namespace physics {
+
+
+class Rigidbody : public System {
+
+};
+
+}  // namespace physics
+}  // namespace h_core
---
+++ core/include/core/physics/rigidbodycomp.h
@@ -0,0 +1,15 @@
+//
+// Created by xavier.olmstead on 9/4/2024.
+//
+
+#pragma once
+
+#define RIGIDBODY_COMPONENT_BITMASK = (1 << 3)
+
+namespace h_core {
+namespace physics {
+class RigidbodyComponent {
+
+};
+}  // namespace physics
+}  // namespace h_core
